6ccc79b9a8f8b319c5f80df89e17488f8e71f64f
maddybaum <madeline.baum94@gmail.com>
2022-09-22 08:31:22 -0500

    Finished project

--- src/Dealership.java
+++ src/Dealership.java
@@ -3,11 +3,12 @@ import java.util.Arrays;
 public class Dealership{
     private Car[] cars;
 
-    public Dealership(){
-        this.cars = new Car[3];
-        System.out.println(Arrays.toString(this.cars));
-
-    }
+    public Dealership(Car[] cars){
+        this.cars = new Car[cars.length];
+        for (int i = 0; i < cars.length; i++) {
+            this.cars[i] = new Car(cars[i]);
+        }
+        }
     public void setCar(Car car, int index){
         this.cars[index] = new Car(car);
     }
@@ -18,4 +19,30 @@ public class Dealership{
         this.cars[index].drive();
         this.cars[index] = null;
     }
+    public int search(String carMake, int budget){
+        for(int i = 0; i < this.cars.length; i++) {
+            if (this.cars[i] == null) {
+                continue;
+            } else if (this.cars[i].getMake().equalsIgnoreCase(carMake) && this.cars[i].getPrice() <= budget) {
+                System.out.println("We found a car in spot " + i + "\n\n" + cars[i].toString());
+                System.out.println("If interested, type yes ");
+                return i;
+            }
+        }
+        System.out.println("We couldn't find any cars");
+        return 404;
+
+        }
+        public String toString(){
+        String temp = " ";
+        for (int i = 0; i <this.cars.length; i++){
+            temp += "\n Parking Spot: " + i + "\n";
+            if(this.cars[i] == null){
+                temp += "Empty for that spot";
+            } else {
+            temp += this.cars[i].toString()+"\n";
+        }}
+        return temp;
+
+        }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
-import org.w3c.dom.ls.LSOutput;
+import java.sql.SQLOutput;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
@@ -6,21 +7,35 @@ public class Main {
         String[] parts = {"tires", "keys"};
 
         Car[] cars = new Car[]{
-                new Car("Nissan", 5000, 2020, "red", parts),
-        new Car("Dodge", 20000, 2021, "green", parts),
+        new Car("Nissan", 5000, 2020, "red", new String[]{"tires", "keys"}),
+        new Car("Dodge", 20000, 2021, "green", new String[]{"tires", "keys"}),
         new Car("Nissan", 5000, 2020, "yellow", new String[]{"tires", "filter"}),
+        new Car("Honda", 7000, 2019, "orange", new String[]{"tires", "filter"}),
+        new Car("Mercedes", 12000, 2015, "black",new String[]{"tires", "filter", "transmission"} ),
     };
+       Dealership dealership = new Dealership(cars);
+        Scanner scan = new Scanner(System.in);
+        System.out.println("\n ****** JAVA DEALERSHIP! ****** \n");
+        System.out.print("Welcome! Enter the type of car you're looking for: ");
+//pick up make
+        String carMake = scan.nextLine();
+        System.out.print("Enter your budget: ");
+//pick up budget
+       int budget = scan.nextInt();
 
-        Dealership dealership = new Dealership();
-        for (int i = 0; i < cars.length; i++) {
-            dealership.setCar(cars[i], i);
-        }
+       int index = dealership.search(carMake, budget);
 
-        Car newCar = dealership.getCar(0);
-        System.out.println(newCar);
-        newCar.setColor("blue");
+       switch (index) {
+           case 404:
+               System.out.println("Please browse through our collection");
+               System.out.println(dealership);
+               break;
 
-        System.out.println(dealership.getCar(0 ));
-        dealership.sell(2);
-    }
-}
+           default:
+               scan.nextLine();
+               String decision = scan.nextLine();
+                 if (decision.equalsIgnoreCase("yes")){
+                   dealership.sell(index);
+               }
+       }
+    }}
